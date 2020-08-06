a79eacf57d9927908a24d976ac93e89afdbf7446
Jaelhp <69092636+Jaelhp@users.noreply.github.com>
2020-08-05 20:51:08 -0500

    Add files via upload

---
+++ Basic-Examples/ExampleBasic.java
@@ -0,0 +1,20 @@
+package Examples;
+/*
+ * Note: This is a example in java with Eclipse IDE.
+ * Author: Jael Emerson Huarca Pallani
+ * version: 1.0
+ * Date: 2020-08-1
+*/
+public class ExampleBasic {
+	
+	// this is the main method prints a message the output on the screen. 
+	public static void main(String[] args) {
+		 //
+		int num1= 8, num2= 0;
+		
+		int result = num1/num2; 
+		//printing result
+		System.out.print(result);
+	}
+
+}
---
+++ Basic-Examples/ExampleBasic1.java
@@ -0,0 +1,15 @@
+package Examples;
+
+import java.util.Scanner;
+
+public class ExampleBasic1 {
+
+	public static void main(String[] args) {
+       @SuppressWarnings("resource")
+	Scanner Sc = new Scanner(System.in);
+       int Num;
+		System.out.print("Ingrese un número:");
+        Num = Sc.nextInt();  
+	}
+
+}
